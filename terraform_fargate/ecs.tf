537a58262538761b4076be6b8f09fd98f6511866
Aadith-tech <aadith323@gmail.com>
2025-12-18 16:16:30 +0530

    fixed naming issue

--- terraform_fargate/ecs.tf
+++ terraform_fargate/ecs.tf
@@ -117,9 +117,9 @@ resource "aws_ecs_task_definition" "strapi_task" {
       healthCheck = {
         command     = ["CMD-SHELL", "curl -f http://localhost:1337/_health || exit 1"]
         interval    = 30
-        timeout     = 5
+        timeout     = 10
         retries     = 3
-        startPeriod = 60
+        startPeriod = 120
       }
     }
   ])
