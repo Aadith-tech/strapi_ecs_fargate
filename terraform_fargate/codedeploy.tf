0abf8def269260c57e49c41d5ab54905bfce2fdc
Aadith-tech <aadith323@gmail.com>
2025-12-22 14:38:23 +0530

    fix name issue

--- terraform_fargate/codedeploy.tf
+++ terraform_fargate/codedeploy.tf
@@ -1,5 +1,5 @@
 resource "aws_codedeploy_deployment_group" "ecs_dg" {
-  app_name              = aws_codedeploy_app.ecs_app.name
+  app_name              = aws_codedeploy_app
   deployment_group_name = "strapi-ecs-dg"
   service_role_arn      = aws_iam_role.code_deploy_role.arn
 
